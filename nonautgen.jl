776571d5785a4f2b59001a1a2a806ca98326b81a
axsk <smartmind23@googlemail.com>
2019-06-14 12:08:33 +0200

    refactor marcus code

--- nonautgen.jl
+++ nonautgen.jl
@@ -1,166 +1,125 @@
-# PART 0: Q Assembly
+using LinearAlgebra, SparseArrays, Arpack, Plots, Random, Parameters
 
-
-# PART 1: Right eigenvectors of Q and PCCA+
-#     ?    The second largest eigenvalue/vector is used to compute
-#         the membership function of the "right" basin of the potential
-#         This function can be time-dependent (if the potential is so)
-#         The membership function is plotted (per time-interval)
-
-# PART 2: Left largest eigenvector of Q and "invariant density"
-#         The "invariant density" is decomposed into parts
-#         which belong to the different time-steps
-#         If sc2==0 then the invariant density is only non-zero for the
-#         last time-step
-#         The density is plotted (per time-interval, blue) and summed up (red)
-
-# PART 3: Computation of the exit rate of the "membership fucntion"
-#         according to new ideas in archive paper
-#         Computation of mean first exit times according to "old school"
-#         with problems of interpretation (moving time)
-#         Plotted: The comparison of the two concepts
-#         only makes sense for sc2==0 or sc==2
-
-
-## SETTINGS
 include("movingpotential.jl")
 include("bickleyjet.jl")
-using LinearAlgebra, SparseArrays, Arpack, Plots, Random
 
-function nonautgen(gen)
-    #scenario of Q-assembly
-    sc2 = 0 # 0=directed time (1=backward, 2=rows&columns, 3=doubly stochastic)
+@with_kw struct AugmentedGenerator
+    gen = MovingPotential() # the underlying generator
+    ntime = 10     # discretization depth which should not influence eigenvalues of Q
+    sc2 = 0            # 0=directed time (1=backward, 2=rows&columns, 3=doubly stochastic)
+end
+
+length(g::AugmentedGenerator) = length(g.gen)
 
 
-    if (sc2 == 3)
-        # for the doubly stochastic approach
-        nr = 2 # 0=sytematically >0=number of randperm-matrices to be added for doubly stochastic
+""" assemble the Q-matrix for a nonautonomous system from their fixed-time generators """
+function generator(c::AugmentedGenerator)
+    @unpack sc2, gen, ntime = c
+
+    t=1/ntime:1/ntime:1
+    eye(n) = Diagonal(ones(n))
+    n = length(gen)
+
+    #assemble big Q-matrix starting from last time step
+    Q=[]
+    for s=ntime:-1:1
+        Qs=generator(t[s], gen)
+        # remove the diagnal entries since we dont want negatives (?)
+        Qs = Qs - Diagonal(Qs)
+
+        # entropy part of Q (?)
+        ds=sum(Qs, dims = 2)
+
+        if (s==1)
+            delta_t=t[s]
+        else
+            delta_t=t[s]-t[s-1]
+        end
+
+        # holding probabilities
+        H = Diagonal(exp.(-delta_t*ds)|>vec)
+
+        s1 = size(Q,1)
+
+        #new row of Q
+        if (s1==0)
+            #first time step = corresponding rate matrix
+            Q=Qs
+        else
+            # with directed time
+            if (sc2==0)
+                Q = [(I-H)*Qs       H*Q[1:n,:]
+                     zeros(s1,n)    Q]
+            end
+
+            # with last row making columns sums equal to 1
+            if (sc2==2)
+                Q = [(I-H)*Qs       H*Q[1:n,:]
+                     zeros(s1,n)    Q]
+                Q[end-n+1:end,1:n]=Qs
+                Q[end-n+1:end,:] = Q[end-n+1:end,:]-Q[1:n,:]
+            end
+        end
     end
 
-    #discretization depth which should not influence eigenvalues of Q
-    timesteps=10  #number of intervals in [0,1]-time (e.g. 10)
-    t=1/timesteps:1/timesteps:1
+    #negative elements are only on the diagonal
+    Q=Q-Diagonal(sum(Q,dims=2)|>vec)
+    Q=sparse(Q)
+end
 
+function analyse(ag::AugmentedGenerator = AugmentedGenerator(), Q=generator(ag))
     # threshold for turning fuzzy sets into hard sets
     thresh=0.3
-
-    ##
-
-    eye(n) = Diagonal(ones(n))
-
-    #sc2 ={0,1,2} assemble big Q-matrix starting from last time step
-    if (sc2<3)
-        Q=[]
-        for s=timesteps:-1:1
-            Qs=generator(t[s], gen)
-            grids=size(Qs,1)
-            ds=sum(Qs, dims = 2)       #and entropy part of Q
-
-            if (s==1)
-                delta_t=t[s]
-            else
-                delta_t=t[s]-t[s-1]
-            end
-            H=Diagonal(exp.(-delta_t*ds)|>vec)  #holding probabilities
-
-            #new row of Q
-            s1=size(Q,1)
-            if (s1==0)
-                Q=Qs #first time step = corresponding rate matrix
-            else
-                #EITHER: with directed time
-                if (sc2==0)
-                    Q=[(I-H)*Qs  H*Q[1:grids,:]
-                    zeros(s1,grids)    Q]
-                end
-
-                #OR: with backward steps based columnwise on same rate matrix
-                if (sc2==1)
-                    block=[]
-                    fact=1/2
-                    for j=s:timesteps-1
-                        block=[block; fact*Qs]
-                        fact=fact*1/2
-                    end
-                    Q=[(eye(grids)-H)*Qs       H*Q[1:grids,:]
-                    block  1/2*Q[:,1:grids]  Q[:,grids+1:end]]
-                end
-
-                #OR: with last row making columns sums equal to 1
-                if (sc2==2)
-                    Q=[(eye(grids)-H)*Qs  H*Q[1:grids,:]
-                    zeros(s1,grids)    Q]
-                    Q[end-grids+1:end,1:grids]=Qs
-                    Q[end-grids+1:end,:] = Q[end-grids+1:end,:]-Q[1:grids,:]
-                end
-            end
-        end
-    end
-
-
-    # sc2==3: Assemble big Q-matrix according to doubly stochastic matrix
-    if (sc2==3)
-        #generate doubly stochastic matrix R
-        R=zeros(timesteps, timesteps)
-        weights=rand(1,nr)
-        weights=weights/sum(weights)
-        for i=1:nr
-            E=eye(timesteps)
-            R=R+weights[i]*E[1:timesteps,randperm(timesteps)]
-        end
-        if (nr==0)
-            R=1/timesteps*ones(timesteps,timesteps)
-        end
-
-        #Fill columns of Q with weighted Qs
-        Qs=infinit_generator(t[1])
-        grids=size(Qs,1)
-        Q=zeros(timesteps*grids, timesteps*grids)
-        for i=1:timesteps
-            Qs=infinit_generator(t[i])
-            grids=size(Qs,1)
-            ds=sum(Qs,dims=2)       #and entropy part of Q
-
-            for j=1:timesteps
-                Q[(j-1)*grids+1:j*grids,(i-1)*grids+1:i*grids]=R[j,i]*Qs
-            end
-        end
-    end
-
-    Q=Q-Diagonal(sum(Q,dims=2)|>vec) #negative elements are only on the diagonal
-    Q=sparse(Q)
+    ntime = ag.ntime
+    n = length(ag)
 
     spectrum, _ = eigs(Q-0.1*I,nev=6,which=:SM)
     spectrum = spectrum .+ 0.1
 
-    #PCCA+ based on second largest eigenvalue/vector
+    # PART 1: PCCA+ based on second largest eigenvalue/vector
+    #         The second largest eigenvalue/vector is used to compute
+    #         the membership function of the "right" basin of the potential
+    #         This function can be time-dependent (if the potential is so)
+    #         The membership function is plotted (per time-interval)
+
     v, u = eigs(Q-0.1*I, nev=2, which=:SM)
-    u = u*sign(u[grids,2]) # such that chi is the "right part"
+    u = u*sign(u[n,2]) # such that chi is the "right part"
     u2 = real(u[:,2]) # Alex: the real is adhoc, correct?)
     chi=1/(maximum(u2)-minimum(u2))*(u2.-minimum(u2))
 
     #plot membership furnction of the right cluster per time step
     plot(title="membership of right cluster per time")
-    for i=1:timesteps
-        plot!(chi[(i-1)*grids+1:i*grids])
+    for i=1:ntime
+        plot!(chi[(i-1)*n+1:i*n])
     end
     plot!() |> display
 
-    # "invariant density"
+    # PART 2: Left largest eigenvector of Q and "invariant density"
+    #         The "invariant density" is decomposed into parts
+    #         which belong to the different time-steps
+    #         If sc2==0 then the invariant density is only non-zero for the
+    #         last time-step
+    #         The density is plotted (per time-interval, blue) and summed up (red)
+
     v, u = eigs(Q'-0.1*I, nev=2,which=:SM)
     density = u[:,1] / sum(u[:,1])
 
     #plot "invariant density" density per time step
     plot(title="invariant density per time")
-    sumdens=zeros(grids,1)
-    for i=1:timesteps
-        dd = density[(i-1)*grids+1:i*grids] |> real
+    sumdens=zeros(n,1)
+    for i=1:ntime
+        dd = density[(i-1)*n+1:i*n] |> real
         plot!(dd)
         sumdens=sumdens+dd
     end
     plot!(sumdens,color="red") |> display
 
-    # Exit rate estimation according to Natalia Ernst
+    # PART 3: Computation of the exit rate of the "membership fucntion"
+    #         according to new ideas in archive paper
+    #         Computation of mean first exit times according to "old school"
+    #         with problems of interpretation (moving time)
+    #         Plotted: The comparison of the two concepts
+    #         only makes sense for sc2==0 or sc==2
 
     u2 = real(u[:,2])
     rate = -spectrum[2]*maximum(u2)/(maximum(u2)-minimum(u2)) |> real
@@ -175,7 +134,6 @@ function nonautgen(gen)
 
     #plot mean first exit times from both concepts (comparison)
     plot(title="mean first exit time comparison")
-    plot!(chi[grids+1:end]/rate,mfpt[grids+1:end]) #,"y.")
-    plot!(chi[1:grids]/rate,mfpt[1:grids]) |> display #,"ko")
-
+    plot!(chi[n+1:end]/rate,mfpt[n+1:end]) #,"y.")
+    plot!(chi[1:n]/rate,mfpt[1:n]) |> display #,"ko")
 end
