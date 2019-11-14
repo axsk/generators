109a4d29719fd3f290527a38778f6031a028b136
axsk <smartmind23@googlemail.com>
2019-11-14 11:37:04 +0100

    remove old projection
    the one which was mapping only to the start of the time intervals

--- embeddedspacetime.jl
+++ embeddedspacetime.jl
@@ -1,38 +1,5 @@
 using Arpack, LinearAlgebra
 using SparseArrays
-using Plots
-# actually it should be embedded augmented / space-time markov chain right?
-# embedded space-time jump chain (estjc)
-
-function augmentedembeddedmatrix(process, ts)
-    ntime = length(ts)
-    n  = length(process)
-
-    dt = vcat(diff(ts), Inf) # boundary open to the right
-    T = spzeros(n*ntime, n*ntime)
-
-    function timeslice(i,j)
-        view(T, (i-1)*n + 1 : (i-1)*n + n, (j-1)*n + 1 : (j-1)*n + n)
-    end
-
-    Q = [generatormatrix(process, t) for t in ts]
-    H = [Diagonal(exp.(dt[i] * diag(Q[i]))) for i in 1:ntime] # prob not to jump (hold) in timeframe 
-    L = [I - H for H in H] # prob to jump (leave) inside timeframe
-    L[end] = Diagonal(ones(n))
-
-    R = [I - Diagonal(Q)\Q for Q in Q] # jump matrix
-
-    for i = 1:ntime
-        for j = i:ntime
-            if i == j
-                timeslice(i,j) .= L[i] * R[i]
-            else
-                timeslice(i,j) .= L[j] * prod(H[k] for k=i:j-1) * R[j]
-            end
-        end
-    end
-    T
-end
 
 function galerkin(process, ts)
     n = length(process)
