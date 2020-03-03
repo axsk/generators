11cdfcb976956136c874d2c05b2003f1f832df7e
axsk <smartmind23@googlemail.com>
2020-03-03 12:54:35 +0100

    replace matrix inverse with 'mysolve'

--- src/commitor.jl
+++ src/commitor.jl
@@ -1,26 +1,46 @@
 using SparseArrays
 
 """ Given a galerkin matrix of size (nm)x(nm), compute the elementary backward commitors for the (n) space-points over the (m) time spans
-Returns a matrix whose `n` columns are the (nm) space-time entries of the `n` commitors """
+Returns a matrix whose `n` columns are the (nm+n) space-time entries of the `n` commitors """
 function commitors(g, n)
-    C = g - I
-    nm = size(C, 1)
-    bnd = nm - n
+    nm = size(g, 1)
+    A = spzeros(nm+n, nm+n)
+    A[1:nm, 1:nm] = g-I # gx = x <=> (g-I)x = 0
 
-    # fix non-transitions by assigning them to the target set
-    s = sum(C, dims=2)
+    # assing non-transitions to the target set
+    s = sum(A, dims=2)
     for i in 1 : nm
-        C[i, nm - n + (i-1)%n + 1] = -s[i]
+        A[i, nm + (i-1)%n + 1] = -s[i]
     end
 
+    b = spzeros(nm+n,n)
     # set boundary condition
-    for i in bnd+1 : nm
-        C[i,:] .= 0
-        C[i,i] = 1
+    for i in 1:n
+        A[nm+i,nm+i] = 1
+        b[nm+i,i]    = 1
     end
 
-    term = spzeros(nm,n)
-    term[nm-n+1:end, :] = sparse(I, n,n)
+    b[nm+1:end, :] .= sparse(I, n,n)
 
-    cs = C\collect(term)
+    # replace the solve by own solver
+    #cs = A\collect(b) # we need dense b, since Julia does not yet support sparse RHS
+
+    m = Int(nm/n)
+    mysolve(A, n, m+1)
+end
+
+# solve Ax=b for x where b has the terminal structure (Id on the bottom and 0 else)
+function mysolve(A, n,m)
+    # split A into timeslices
+    As = [A[i*n .+ (1:n), j*n .+ (1:n)] for i=0:m-1, j=0:m-1]
+    xs = Array{Matrix{Float64}}(undef, m)
+    xs[end] = Matrix(I, n, n) # boundary condition
+    for i=m-1:-1:1
+        b = zeros(n,n)
+        for j=i+1:m
+            b -= As[i,j] * xs[j]
+        end
+        xs[i] = As[i,i] \ b
+    end
+    vcat(xs...)
 end
